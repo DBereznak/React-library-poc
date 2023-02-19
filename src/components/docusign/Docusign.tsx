f6af1dadcd56389b7726a26b41ef9862ffeefc59
dbereznak <dbereznak@hotmail.com>
2023-02-19 17:05:52 -0500

    first push

---
+++ src/components/docusign/Docusign.tsx
@@ -0,0 +1,29 @@
+import React, {useState} from "react";
+
+const Docusign = () => {
+
+    const [ userLoggedIn, setLogIn ] = useState(false);
+
+    function logInHandler(event: React.MouseEvent){
+        event.preventDefault();
+        setLogIn(!userLoggedIn);
+    } 
+
+    if(userLoggedIn){
+        return (
+            <div>
+                <h1>Here is the document you need to sign</h1>
+                <a href="/pdf/pdf_health_first.pdf" download> Health First PDF</a>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                <button onClick={logInHandler}>Login</button>
+            </div>
+        )
+    }
+
+}
+
+export default Docusign;
